ff622b85fd8b2eaeda644147589672d7bf54305c
seemix <91602641+seemix@users.noreply.github.com>
2025-08-23 20:34:30 +0300

    first vps 1.02

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
-const express = require('express');
 
-// Підключаємо всі хендлери бота
 const startHandler = require('./src/handlers/start');
 const mainMenuHandler = require('./src/handlers/mainMenu');
 const productsHandler = require('./src/handlers/products');
@@ -10,7 +8,6 @@ const checkoutHandler = require('./src/handlers/checkout');
 const ordersHandler = require('./src/handlers/orders');
 const bot = require('./src/bot');
 
-// Ініціалізуємо обробники
 startHandler(bot);
 mainMenuHandler(bot);
 productsHandler(bot);
@@ -19,16 +16,3 @@ checkoutHandler(bot);
 ordersHandler(bot);
 
 console.log('🤖 Bot has started');
-
-// --- Додаємо простий сервер для Render ---
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send('🤖 Bot is running');
-});
-
-// Render потребує відкритий порт
-const PORT = process.env.PORT || 3017;
-app.listen(PORT, () => {
-    console.log(`🌐 Web service running on port ${PORT}`);
-});
